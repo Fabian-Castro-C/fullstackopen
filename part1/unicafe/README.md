f410c00466ba5e79626077f9c61b0bbf456f2dc9
Fabian-Castro-C <fabian.castro.c@ug.uchile.cl>
2025-03-16 17:36:39 -0300

    initialize Unicafe project

---
+++ part1/unicafe/README.md
@@ -0,0 +1,3 @@
+# Unicafe Exercise
+
+This project covers exercises 1.6 to 1.11 of the Full Stack Open course.
---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <>
+      <h1>Unicase</h1>
+    </>
+  )
+}
+
+export default App
